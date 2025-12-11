45663ff1782fc971351b3a8bfd498abeecd4345a
YairVera <yairvera44@gmail.com>
2025-12-11 09:22:50 -0600

    exercise-northwind traders api

--- src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -1,13 +1,16 @@
 package com.pluralsight.NorthwindTradersAPI;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping(path = "home")
-    public String renderHomePage(){
-        return "Hello World";
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+
+    public String index(@RequestParam(defaultValue = "World") String country) {
+        return "Hello" + country;
     }
 }
